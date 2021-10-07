905b383aaa02ebb4c76ac3385314ce9b168df3d3
ransomedesign <info@ransomedesign.co.uk>
2021-10-07 16:05:58 +0100

    module 2

--- src/app.js
+++ src/app.js
@@ -9,9 +9,46 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
+// Read the account data.
+const accountData = fs.readFileSync("src/json/accounts.json", {
+  encoding: "UTF8",
+});
+const accounts = JSON.parse(accountData);
+// Read the user data.
+const userData = fs.readFileSync("src/json/users.json", { encoding: "UTF8" });
+const users = JSON.parse(userData);
+
+// **** Routes ****
+// Index.
 app.get("/", (req, res, next) => {
-  res.render("index", { title: "Index" });
+  res.render("index", {
+    title: "Account Summary",
+    accounts: accounts,
+  });
+});
+// Savings.
+app.get("/savings", (req, res, next) => {
+  res.render("account", {
+    account: accounts.savings,
+  });
+});
+// Checking.
+app.get("/checking", (req, res, next) => {
+  res.render("account", {
+    account: accounts.checking,
+  });
+});
+// Credit.
+app.get("/credit", (req, res, next) => {
+  res.render("account", {
+    account: accounts.credit,
+  });
+});
+// Profile route
+app.get("/profile", (req, res, next) => {
+  res.render("profile", {
+    user: users[0],
+  });
 });
 
 app.listen(3000, () => {
